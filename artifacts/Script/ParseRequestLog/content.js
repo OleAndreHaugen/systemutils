43ea2b8afa116e63cfd67a666b039df8fb6ccbe7
Ole Andre <ole-andre.haugen@neptune-software.com>
2024-06-24 13:25:00 +0200

    Added filter and stats

--- artifacts/Script/ParseRequestLog/content.js
+++ artifacts/Script/ParseRequestLog/content.js
@@ -1,13 +1,15 @@
 const fs = modules.fs;
 
-// const fileName = "C:/Planet9/planet9/log/requests/2024-06-24-planet9.log";
-
 const fileName = "./log/requests/" + req.query.fileName;
 const fileContent = fs.readFileSync(fileName, "utf8");
 const fileFormatted = fileContent.split("\n").reverse();
 
 let logContent = [];
 let topUrl = {};
+let reqOK = 0;
+let reqNOK = 0;
+let totReq = 0;
+let totDuration = 0;
 
 try {
     fileFormatted.forEach((element) => {
@@ -19,17 +21,26 @@ try {
             log = JSON.parse(element);
         } catch (e) {}
 
-        logContent.push(log);
-
         const path = log.message.request.host + log.message.request.url;
 
         if (!topUrl[path])
             topUrl[path] = {
                 requests: 0,
                 duration: 0,
+                errors: 0
             };
         topUrl[path].requests++;
         topUrl[path].duration = topUrl[path].duration + log.message.response.duration;
+
+        if (log.message.response.statusCode === 200) {
+            reqOK++;
+        } else {
+            reqNOK++;
+            topUrl[path].errors++;
+        }
+
+        totReq++;
+        totDuration = totDuration + log.message.response.duration;
     });
 
     const keyValueArray = Object.entries(topUrl);
@@ -43,14 +54,18 @@ try {
             url: key,
             requests: sortedJsonObject[key].requests,
             duration: parseFloat(sortedJsonObject[key].duration.toFixed(2)),
+            errors: sortedJsonObject[key].errors,
         });
     }
 
-    console.log(topUrlList);
-
     result.data = {
-        logContent: logContent,
         topUrlList: topUrlList,
+        requestInfo: {
+            reqOK,
+            reqNOK,
+            totReq,
+            totDuration,
+        },
     };
 } catch (e) {
     result.data = {
